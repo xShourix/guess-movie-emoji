a8e75b380dbd67c7916ee057ca56f185bf0207c0
Wiktoria <wiktoriasobczyk13@gmail.com>
2026-05-20 22:52:14 +0200

    chore: create initial project structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/global.css';
 import reportWebVitals from './reportWebVitals';
 reportWebVitals(console.log);
 const root = ReactDOM.createRoot(document.getElementById('root'));
