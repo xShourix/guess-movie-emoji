777891e817be1f92ee1cf78d550a285652500439
Wiktoria <wiktoriasobczyk13@gmail.com>
2026-06-09 23:40:10 +0200

    change of riddleApi structure

--- api/riddleApi.php
+++ api/riddleApi.php
@@ -7,20 +7,30 @@ $conn = new mysqli("localhost", "root", "", "emoji_quiz");
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$result = $conn->query(
-    "INSERT INTO riddles (title, answer)
-     VALUES ('{$data['title']}', '{$data['answer']}')"
-);
+switch($_SERVER['REQUEST_METHOD']) {
+    case "POST":
+        $result = $conn->query(
+            "INSERT INTO riddles (title, answer)
+            VALUES ('{$data['title']}', '{$data['answer']}')"
+        );
 
-if ($result) {
-    echo json_encode([
-        "success" => true
-    ]);
-} else {
-    echo json_encode([
-        "success" => false,
-        "message" => "Adding riddle failed: " . $conn->error
-    ]);
+        if ($result) {
+            echo json_encode([
+                "success" => true
+            ]);
+        } else {
+            echo json_encode([
+                "success" => false,
+                "message" => "Adding riddle failed: " . $conn->error
+            ]);
+        }
+        break;
+    default:
+        echo json_encode([
+            "success" => false,
+            "message" => "Method not allowed"
+        ]);
 }
+
 
 $conn->close();
--- src/pages/MovieDatabase.jsx
+++ src/pages/MovieDatabase.jsx
@@ -1,12 +1,46 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function MovieDatabase() {
+
+  const [titleSearch, setTitleSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await fetch("http://localhost:3000/movies");
+        if (!response.ok) {
+          throw new Error("Could not fetch movies");
+        }
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
-      <div className="dRowSpacebetween">
+      <section className="dRowSpacebetween">
         <h1><span className="thinText">Riddle</span> database</h1>
         <Link to="/add" className="button">Add new</Link>
-      </div>
+      </section>
+      <section className="movieDatabase">
+        <input className="search-input" type="text" placeholder="Search..." value={titleSearch} onChange={e => setTitleSearch(e.target.value)} />
+        <div className="movieResults">
+          {movies
+            .filter(movie => movie.title.toLowerCase().includes(titleSearch.toLowerCase()))
+            .map(movie => (
+              <Link to={`/movie/${movie.id}`} key={movie.id} className="search-item">
+                <h2>{movie.title}</h2>
+                <p>{movie.year}</p>
+              </Link>
+            ))}
+        </div>
+      </section>
     </main>
   );
 }
